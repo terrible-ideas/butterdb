794c17439711572685e395a82a91ff9424e112b3
Nick Johnstone <ncwjohnstone@gmail.com>
2013-12-11 00:58:00 +1300

    Starting building model registration

--- fuckitdb/fuckit.py
+++ fuckitdb/fuckit.py
@@ -8,19 +8,39 @@ class Database(object):
         self.name = name
         self.client = gspread.login(username, password)
         self.db = self.client.open(name)
-        self.model = Model
+
+    def register(self):
+        """Registers a model for storage in the database"""
+        def decorator(f):
+            f.database = self
+            f.data = self.db.worksheet(f.__name__)
+            return f
+        return decorator        
+
+
+class ModelMetaclass(type):
+    def __call__(cls, *args, **kwargs):
+        """Called when you call MyNewClass() """
+        obj = type.__call__(cls, *args, **kwargs)
+        obj._post_init()
+        return obj
 
 
 class Model(object):
     """The base object for representing cell data as an object"""
+    __metaclass__ = ModelMetaclass
+
     def _get_own_sheet(self):
         return self.database.__getattr__(self.__name__)
 
     def __setattr__(self, attr, val):
-        return self._get_own_sheet.__setattr__(attr, val)
+        pass
 
     def __getattr__(self, attr):
-        self.database.
+        pass
+
+    def _post_init(self):
+        self._id = self.database.assign_id(self)
 
     def register(self, database):
         """Registers the model for storage in the database"""
