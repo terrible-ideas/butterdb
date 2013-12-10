b0239415843bba07d64bb107aec667acd3a834c8
Nick Johnstone <ncwjohnstone@gmail.com>
2013-12-11 00:40:51 +1300

    Added tests for database and master

---
+++ tests/test_database.py
@@ -0,0 +1,21 @@
+import fuckitdb
+import os
+
+email = os.environ["FUCKITDB_EMAIL"]
+password = os.environ["FUCKITDB_PASSWORD"]
+
+
+class TestDB(object):
+
+    def test_open(self):
+        """Tests opening a Database"""
+        database = create_test_db()
+
+        assert database != None
+
+def create_test_db():
+    database = fuckitdb.Database(name="TestDB",
+                                 username=email,
+                                 password=password)
+
+    return database
---
+++ tests/test_model.py
@@ -0,0 +1,21 @@
+import fuckitdb
+import test_database
+
+database = test_database.create_test_db()
+
+class FooModel(fuckitdb.Model):
+    def __init__(self, foo, bar):
+        self.foo = foo
+        self.bar = bar
+        self.register(database)
+
+
+class TestModel(object):
+    def test_attrs(self):
+        foo, bar = "baz", "fro"
+        model = FooModel(foo, bar)
+
+        assert model.foo == foo
+        assert model.bar == bar
+
+
