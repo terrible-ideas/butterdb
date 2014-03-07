78b982ae38d1c547fbc1aebb379ec0486c1416e8
Nick Johnstone <ncwjohnstone@gmail.com>
2014-03-07 14:01:42 +1300

    Incremented version

--- setup.py
+++ setup.py
@@ -15,12 +15,13 @@ if sys.argv[-1] == 'publish':
     sys.exit()
 
 readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='fuckitdb',
-    version='0.1.1',
+    version='0.1.2',
     description='fuckitdb is a Python ORM for Google Drive Spreadsheets.',
-    long_description=readme,
+    long_description=readme + '\n\n' + history,
     author='Nick Johnstone',
     author_email='ncwjohnstone@gmail.com',
     url='https://github.com/Widdershin/fuckitdb',
